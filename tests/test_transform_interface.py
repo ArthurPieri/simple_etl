53e1758f11c56115c6bbd247a12b0b521953f2a8
arthurpieri <git@arthurpieri.com>
2023-09-13 14:58:18 -0300

    tests: finishing test coverage over 90%

--- tests/test_transform_interface.py
+++ tests/test_transform_interface.py
@@ -35,13 +35,53 @@ class TestTransformPostgres:
         assert data[0]["first_name"] == "John"
         assert "first_name_new" not in data[0].keys()
 
-    def test_treat_column_names(self, obj, fixture_extracted_data):
-        data = fixture_extracted_data
-        data[0]["create.$date"] = "2020-10-19T00:00:00.000Z"
-        data[1]["create.$date"] = "2020-10-19T00:00:00.000Z"
+    def test_treat_column_names(self, obj):
+        data = [
+            {
+                "id.$oid": "5f8d0c5c0b3a7f7e9d5f0e9d",
+                "last_name": "Doe",
+                "email": "",
+                "first_name": "John",
+                "create.$date": "2020-10-19T00:00:00.000Z",
+                "user.$oid": "teste",
+                "purchase.$date": "2020-10-19T00:00:00.000Z",
+                "teste.dot": "teste",
+                "remove$": "teste",
+                "name with spaces": "teste",
+                "something_with_ç_diactrics": "teste",
+            }
+        ]
         columns, data = obj._treat_column_names(data=data)
-        assert columns == {"id", "last_name", "email", "first_name", "create_date"}
-        # test every if in treat_column_names
+        assert columns == {
+            "id",
+            "last_name",
+            "email",
+            "first_name",
+            "create_date",
+            "user_id",
+            "purchase_date",
+            "teste_dot",
+            "remove",
+            "name_with_spaces",
+            "something_with_c_diactrics",
+        }
 
-    def test_get_python_types(self):
-        ...
+    def test_get_python_types(self, obj):
+        columns = {"id", "last_name", "email", "first_name", "create_date"}
+        data = [
+            {
+                "id": "5f8d0c5c0b3a7f7e9d5f0e9d",
+                "last_name": "Doe",
+                "email": "",
+                "first_name": "John",
+                "create_date": "2020-10-19T00:00:00.000Z",
+            },
+        ]
+        cols_and_types = obj._get_python_types(columns=columns, data=data)
+        assert cols_and_types == {
+            "id": [str],
+            "last_name": [str],
+            "email": [str],
+            "first_name": [str],
+            "create_date": [str],
+        }
