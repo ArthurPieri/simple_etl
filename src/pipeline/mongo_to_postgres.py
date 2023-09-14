9ed688ab8c182ec58fe55082fb2114e57f4f63a4
arthurpieri <git@arthurpieri.com>
2023-09-14 00:12:31 -0300

    feat: init mongo to postgres pipeline

--- src/pipeline/mongo_to_postgres.py
+++ src/pipeline/mongo_to_postgres.py
@@ -1,1 +1,15 @@
-# check if it is possible to get a list and create a new table with it
+# pylint: disable=relative-beyond-top-level
+
+from ..extract.extract_mongodb import FromMongodb
+from ..transform.transform_to_postgres import TransformPostgres
+from ..load.load_postgres import ToPostgres
+
+from .interface.pipeline_interface import PipelineInterface
+
+
+class MongoToPostgres(PipelineInterface, FromMongodb, TransformPostgres, ToPostgres):
+    """'
+    This class is used to run the pipeline from MongoDB to Postgres
+    """
+
+    # check if it is possible to get a list and create a new table with it
